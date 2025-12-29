dc0f2f9c9099a6735788b324320292741e0d046d
JAnMorss <johnanthonymorales124@gmail.com>
2025-12-29 20:19:06 +0800

    update dependencies and configure Vite for SVG imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
+import svgr from "vite-plugin-svgr"
+import path from 'path'
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [svgr(), tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
