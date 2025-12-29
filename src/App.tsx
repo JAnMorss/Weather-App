7ced8a9231484f91e65f14dadcf9ded21b6c5b34
JAnMorss <johnanthonymorales124@gmail.com>
2025-12-29 20:21:23 +0800

    update configuration and application files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import DailyForecast from "./components/cards/DailyForecast"
 import HourlyForecast from "./components/cards/HourlyForecast"
 import CurrentWeather from "./components/cards/CurrentWeather"
+import AdditionalInfo from "./components/cards/AdditionalInfo"
+import Map from "./components/Map"
+import { useState } from "react"
+import type { Coords } from "./types"
+import LocationDropdown from "./components/dropdowns/LocationDropdown"
 
 function App() {
+  const [coords, setCoords] = useState<Coords>({ lat: 10.3157, lon: 123.8854 })
+
+  const onMapClick = (lat: number, lon: number) => {
+    setCoords({ lat, lon })
+  }
 
   return (
     <div className="flex flex-col gap-8">
-      <CurrentWeather />
-      <HourlyForecast />
-      <DailyForecast />
+      <LocationDropdown />
+      <Map coords={coords} onMapClick={onMapClick} />
+      <CurrentWeather coords={coords} />
+      <HourlyForecast coords={coords} />
+      <DailyForecast coords={coords} />
+      <AdditionalInfo coords={coords} />
     </div>
   )
 }
--- src/api.ts
+++ src/api.ts
@@ -3,8 +3,8 @@ import WeatherSchema from "./schemas/weatherSchema";
 export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
   const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}` +
     `&current_weather=true` +
-    `&hourly=temperature_2m,relative_humidity_2m,precipitation,windspeed_10m,weathercode` +
-    `&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,sunrise,sunset,weathercode` +
+    `&hourly=temperature_2m,relative_humidity_2m,precipitation,windspeed_10m,weathercode,winddirection_10m,cloudcover,pressure_msl` +
+    `&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,sunrise,sunset,weathercode,uv_index_max` +
     `&timezone=auto`;
 
   const res = await fetch(url);
@@ -13,4 +13,18 @@ export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
   const weather = WeatherSchema.parse(data);
 
   return weather;
+}
+
+export async function getGeoCode(location: string) {
+  const url = `https://nominatim.openstreetmap.org/search?q=${location}&format=json&limit=1`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (data.length === 0) {
+    throw new Error('Location not found');
+  }
+
+  const { lat, lon } = data[0];
+  return { lat: parseFloat(lat), lon: parseFloat(lon) };
 }
